a36df266cd0b266c23b7a2f47164c770ef8212c9
sqooid <github@sl.thesqooid.com>
2024-10-15 08:59:39 +1100

    create install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+logrotate_config="/etc/logrotate.d/cloudflare-ddns"
+if [ -f "${logrotate_config}" ]; then
+  echo "Log rotate config ${logrotate_config} already exists"
+  exit 1
+fi
+
+dir=$(cd $(dirname 0) && pwd)
+
+log_path="/var/log/cloudflare-ddns.log"
+sudo touch "${log_path}" && sudo chmod 666 "${log_path}"
+
+
+# Install log rotate
+cat << EOF | sudo tee "${logrotate_config}"
+${log_path} {
+  daily
+  rotate 1
+}
+EOF
+
+# Install crontab
+(crontab -l 2>/dev/null; echo "* * * * * cd ${dir} && ./run.sh >> /var/log/cloudflare-ddns.log 2>&1") | crontab -
